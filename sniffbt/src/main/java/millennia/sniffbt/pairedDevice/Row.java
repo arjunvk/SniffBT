2d7991f766db7e2d905f9e4805f041bdd6a2ea03
arjun.vijayakumar <arjun.vijayakumar@ao.uscourts.gov>
2016-09-08 20:07:54 -0400

    Add getDeviceMajorClass and getDevicePersistence

--- sniffbt/src/main/java/millennia/sniffbt/pairedDevice/Row.java
+++ sniffbt/src/main/java/millennia/sniffbt/pairedDevice/Row.java
@@ -5,15 +5,18 @@ import android.bluetooth.BluetoothDevice;
 import java.io.Serializable;
 
 public class Row implements Serializable {
-    //private BluetoothDevice device;
     private String name;
     private String address;
     private boolean blnIsCBChecked;
+    private boolean blnIsPersistent;
+    private int intBluetoothMajorClass;
 
     public Row(BluetoothDevice device, boolean blnCBChecked) {
         this.name = device.getName();
         this.address = device.getAddress();
         this.blnIsCBChecked = blnCBChecked;
+        this.blnIsPersistent = false;
+        this.intBluetoothMajorClass = device.getBluetoothClass().getMajorDeviceClass();
     }
 
     public String getDeviceAddress() { return this.address;}
@@ -27,4 +30,14 @@ public class Row implements Serializable {
     public boolean isCBChecked() {
         return this.blnIsCBChecked;
     }
+
+    public void setDevicePersistence(boolean blnIsPersistent) {
+        this.blnIsPersistent = blnIsPersistent;
+    }
+
+    public boolean getDevicePersistence() {
+        return this.blnIsPersistent;
+    }
+
+    public int getDeviceMajorClass() { return this.intBluetoothMajorClass; }
 }
