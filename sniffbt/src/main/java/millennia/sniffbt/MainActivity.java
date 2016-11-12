086c39dcbdea6156ae5690dd167ca3b3b66ae678
Arjun Vijayakumar <arjunvk@gmail.com>
2016-11-12 15:56:36 -0500

    Fix SniffBT turn on issue when Scan frequency is not set

--- sniffbt/src/main/java/millennia/sniffbt/MainActivity.java
+++ sniffbt/src/main/java/millennia/sniffbt/MainActivity.java
@@ -185,28 +185,30 @@ public class MainActivity extends AppCompatActivity{
                 @Override
                 public void onClick(View v) {
                     if(cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Scan_Frequency_In_Seconds), int.class) != null) {
-                        if(((String) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Sniff_BT_OnOff), String.class)).equalsIgnoreCase("OFF")) {
-                            // Turn on Sniff BT
-                            arrPairedDevicesList = (Row[]) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Paired_Devices), Row[].class);
+                        if((Integer)cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Scan_Frequency_In_Seconds), int.class) != 0) {
+                            if(((String) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Sniff_BT_OnOff), String.class)).equalsIgnoreCase("OFF")) {
+                                // Turn on Sniff BT
+                                arrPairedDevicesList = (Row[]) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Paired_Devices), Row[].class);
 
-                            // Start service to listen to BT
-                            Log.i(TAG, "Starting Intent Service to start SniffBT scheduler...");
-                            intentListenBT.putExtra("PairedDevicesList", cf.serialize(arrPairedDevicesList));
-                            getApplicationContext().startService(intentListenBT);
-                            mSniffBT.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_action_sniff_bt_on));
-                            cf.showSnackBar(v, "SniffBT has been turned ON", Snackbar.LENGTH_SHORT);
-                        }
-                        else if(((String) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Sniff_BT_OnOff), String.class)).equalsIgnoreCase("ON")) {
-                            // Turn off Sniff BT
-                            Log.i(TAG, "Starting Intent Service to stop SniffBT scheduler...");
-                            getApplicationContext().startService(intentListenBT);
+                                // Start service to listen to BT
+                                Log.i(TAG, "Starting Intent Service to start SniffBT scheduler...");
+                                intentListenBT.putExtra("PairedDevicesList", cf.serialize(arrPairedDevicesList));
+                                getApplicationContext().startService(intentListenBT);
+                                mSniffBT.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_action_sniff_bt_on));
+                                cf.showSnackBar(v, "SniffBT has been turned ON", Snackbar.LENGTH_SHORT);
+                            }
+                            else if(((String) cf.getSharedPreferences(appPrefs, getString(R.string.SH_PREF_Sniff_BT_OnOff), String.class)).equalsIgnoreCase("ON")) {
+                                // Turn off Sniff BT
+                                Log.i(TAG, "Starting Intent Service to stop SniffBT scheduler...");
+                                getApplicationContext().startService(intentListenBT);
 
-                            mSniffBT.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_action_sniff_bt_off));
-                            cf.showSnackBar(v, "SniffBT has been turned OFF", Snackbar.LENGTH_SHORT);
+                                mSniffBT.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_action_sniff_bt_off));
+                                cf.showSnackBar(v, "SniffBT has been turned OFF", Snackbar.LENGTH_SHORT);
+                            }
                         }
-                    }
-                    else {
-                        cf.showSnackBar(v, "Set the frequency of scan", Snackbar.LENGTH_SHORT);
+                        else {
+                            cf.showSnackBar(v, "Set the frequency of scan", Snackbar.LENGTH_SHORT);
+                        }
                     }
                 }
             });
